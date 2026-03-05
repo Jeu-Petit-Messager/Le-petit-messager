763ede64d7afd8a171b112f02da29358087c7f3d
Tseng, Yun Yeh <e2483206@cmaisonneuve.qc.ca>
2026-03-05 16:38:54 -0500

    menu finit

---
+++ Assets/scripts/nouvellePartie.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class nouvellePartie : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SceneManager.LoadScene("sceneJeu");
+    }
+}
