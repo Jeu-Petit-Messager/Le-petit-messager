1a95cc1354a42cb4e11985983117c5ff3de1c8c4
Tseng, Yun Yeh <e2483206@cmaisonneuve.qc.ca>
2026-03-07 20:58:31 -0500

    fais le bouton controles cliquable et retourner

---
+++ Assets/scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject controls;
+
+    void Start()
+    {
+        menu.SetActive(true);
+        controls.SetActive(false);
+    }
+
+    void Update()
+    {
+        // Si on appuie sur Échap et que le menu contrôle est ouvert
+        if (controls.activeSelf && Input.GetKeyDown(KeyCode.Escape))
+        {
+            RetourMenu();
+        }
+    }
+
+    // Quand on clique sur le bouton "Contrôles"
+    public void OpenControls()
+    {
+        menu.SetActive(false);
+        controls.SetActive(true);
+    }
+
+    // Quand on clique sur "Retourner"
+    public void RetourMenu()
+    {
+        controls.SetActive(false);
+        menu.SetActive(true);
+    }
+}
