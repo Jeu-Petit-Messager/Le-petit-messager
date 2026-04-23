82e02a13e9ca046247ecc59d96fab61a285caa66
Xavier Rémy <ReivaxCode4@gmail.com>
2026-04-23 16:59:43 -0400

    X-diag29 : diag pt2
    Tutoriel 5:50

--- Assets/scripts/dialogues/XXPNJ.cs
+++ Assets/scripts/dialogues/XXPNJ.cs
@@ -2,7 +2,31 @@ using UnityEngine;
 
 public abstract class XXPNJ : MonoBehaviour, IInteractable
 {
+    // Lorsqu'un script utilise IInteractable, il doit comporter tous les elements de cette interface
     public string InteractionPrompt => throw new System.NotImplementedException();
 
+    [SerializeField] private SpriteRenderer spriteIndicPNJ;
+
+    public Transform detectTransformGars;
+
+    const float distanceInteraction = 5f;
+
+    private void Start()
+    {
+        
+    }
+
     public abstract void Interact();
+
+    private bool isWithinInteractDistance()
+    {
+        if (Vector3.Distance(detectTransformGars.position, transform.position) < distanceInteraction)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
