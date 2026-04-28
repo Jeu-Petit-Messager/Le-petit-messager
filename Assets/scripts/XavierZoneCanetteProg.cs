92f4929b5db9456172fd373fa2efc94467947be8
Xavier Rémy <ReivaxCode4@gmail.com>
2026-04-28 12:48:00 -0400

    incorporer assets merge

---
+++ Assets/scripts/XavierZoneCanetteProg.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class XavierZoneCanetteProg : MonoBehaviour
+{
+    public static float canetteCollectees;
+
+    public GameObject canette1;
+    public GameObject canette2;
+    public GameObject canette3;
+    public GameObject canette4;
+    public GameObject canette5;
+    public GameObject canette6;
+
+    public AudioSource sonRamassage;
+
+    public GameObject flecheBlanche;
+
+    public void Update()
+    {
+        if(!canette1.activeSelf)
+        {
+            if (canetteCollectees == 1)
+            {
+                canette1.SetActive(true);
+                sonRamassage.Play();
+            }
+        }
+        if (!canette2.activeSelf)
+        {
+            if (canetteCollectees == 2)
+            {
+                canette2.SetActive(true);
+                sonRamassage.Play();
+
+            }
+        }
+        if (!canette3.activeSelf)
+        {
+            if (canetteCollectees == 3)
+            {
+                canette3.SetActive(true);
+                sonRamassage.Play();
+
+            }
+        }
+        if (!canette4.activeSelf)
+        {
+            if (canetteCollectees == 4)
+            {
+                canette4.SetActive(true);
+                sonRamassage.Play();
+
+            }
+        }
+        if (!canette5.activeSelf)
+        {
+            if (canetteCollectees == 5)
+            {
+                canette5.SetActive(true);
+                sonRamassage.Play();
+
+            }
+        }
+        if (!canette6.activeSelf)
+        {
+            if (canetteCollectees == 6)
+            {
+                sonRamassage.Play();
+                canette6.SetActive(true);
+                flecheBlanche.SetActive(false);
+            }
+        }
+    }
+
+}
