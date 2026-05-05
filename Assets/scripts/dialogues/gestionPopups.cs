c25c0e37d6567693f30e9ec5f9ec9f7714a9186b
Xavier Rémy <ReivaxCode4@gmail.com>
2026-05-05 11:18:57 -0400

    X-tuto39 : test sprint
    Le joueur doit courir pour le cinquieme dialogue. Je me demande comment je vais déterminer les textes qui disparaissent par eux même sans clics

--- Assets/scripts/dialogues/gestionPopups.cs
+++ Assets/scripts/dialogues/gestionPopups.cs
@@ -19,9 +19,11 @@ public class gestionPopups : MonoBehaviour
     public float delayAvantAffichage = 0f;
     public float vitesseEcriture = 0.05f;
 
+    // Statut determinantlorsque le texte est en ecriture
     public bool estEnTrainDEcrire;
-
-    // Variables de conditions
+    
+    /* Variables de conditions */
+    // Enregistrer positions souris
     Vector3 sourisPos;
     Vector3 maintientPos;
     public float compteAccroupi;
@@ -29,18 +31,20 @@ public class gestionPopups : MonoBehaviour
     void Start()
     {
         dialogueText.gameObject.SetActive(true);
-        indexListeDiag = 0;
-
-
         dialogueBox.SetActive(false);
         dialogueText.text = "";
 
+        // Le compteur de dialogue commence a zero
+        indexListeDiag = 0;
+
+
         StartCoroutine(LancerDialogue());
-        estEnTrainDEcrire=true;
+        estEnTrainDEcrire = true;
 
         compteAccroupi = 0f;
     }
-    // Coroutine pour lancer le dialogue après un delay, puis ecrire le texte lettre par lettre
+
+    /* Coroutine pour lancer le dialogue après un delay, puis ecrire le texte lettre par lettre */
     IEnumerator LancerDialogue()
     {
         estEnTrainDEcrire = true;
@@ -55,7 +59,8 @@ public class gestionPopups : MonoBehaviour
 
         StartCoroutine(EcrireTexte(listeDiag[indexListeDiag]));
     }
-    // Coroutine pour écrire le texte lettre par lettre
+
+    /* Coroutine pour écrire le texte lettre par lettre */
     IEnumerator EcrireTexte(string msg)
     {
         dialogueText.text = "";
@@ -70,10 +75,30 @@ public class gestionPopups : MonoBehaviour
 
     void Update()
     {
-
+            /* Determiner conditions pour skip dialogue */
             if (estEnTrainDEcrire)
             {
-                if(indexListeDiag == 1)
+                // 1. Test camera
+                if (indexListeDiag == 0)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        maintientPos = Input.mousePosition;
+
+                        if (maintientPos != sourisPos)
+                        {
+                            ChargerTexteEntierEarly();
+                            StartCoroutine(FermerEtLancerMessageAuto());
+                            indexListeDiag++;
+                        }
+
+                    }
+                    else sourisPos = Input.mousePosition;
+
+                }
+
+                // 2. Test accroupissement
+                else if (indexListeDiag == 1)
                 {
                     if (Input.GetKeyDown(KeyCode.LeftControl))
                     {
@@ -83,11 +108,33 @@ public class gestionPopups : MonoBehaviour
                     } 
                 }
 
+                // 3. Test saut
+                else if (indexListeDiag == 2)
+                {
+                    if (Input.GetKeyDown(KeyCode.Space))
+                    {
+                        ChargerTexteEntierEarly();
+                        StartCoroutine(FermerEtLancerMessageAuto());
+                        indexListeDiag++;
+                    }
+                }
+
+                // 5. Test course
+                else if(indexListeDiag == 4)
+                {
+                    if (Input.GetKeyDown(KeyCode.LeftShift))
+                    {
+                        ChargerTexteEntierEarly();
+                    }
+                }
+
+                /* Condition generale, juste clicker */
                 if (Input.GetMouseButtonDown(0))
                 {
                     ChargerTexteEntierEarly();
                 }
 
+                // Le texte finit d'ecrire lorsqu'il devient pareil a la valeur de la liste
                 if(dialogueText.text == listeDiag[indexListeDiag]) estEnTrainDEcrire = false;
 
             }
@@ -95,50 +142,82 @@ public class gestionPopups : MonoBehaviour
             /* Lorsque le texte a fini d'ecrire, voici des conditions de texte de tuto */
             else
             {
-                // Au debut, faire la verif deplacement cam
-                if(indexListeDiag == 0)
+            // 1. faire la verif deplacement cam
+            if (indexListeDiag == 0)
+            {
+
+                // 0 correspond au clic gauche
+                if (Input.GetMouseButtonDown(0))
                 {
+                    // Le maintient est seulement calcule au hold
+                    maintientPos = Input.mousePosition;
 
-                    // 0 correspond au clic gauche
-                    if (Input.GetMouseButtonDown(0))
-                    {
-                        maintientPos = Input.mousePosition;
-                        if(maintientPos != sourisPos)
-                        {
-                            StartCoroutine(FermerEtLancerMessageAuto());
-                            indexListeDiag++;
-                        }
-
-                    }
-                    else
-                    {
-                        sourisPos = Input.mousePosition;
-                    }
-
-                }
-
-                else if (indexListeDiag == 1)
-                {
-
-                    if (Input.GetKeyDown(KeyCode.LeftControl))
-                    {
-                        if (compteAccroupi < 2f) compteAccroupi++;
-                    }
-
-                    if (compteAccroupi == 2f)
+                    // Lorsque la valeur de maintient change de la derniere enregistree, passe au suivant
+                    if (maintientPos != sourisPos)
                     {
                         StartCoroutine(FermerEtLancerMessageAuto());
                         indexListeDiag++;
                     }
+
                 }
                 else
                 {
-                    if (Input.GetMouseButtonDown(0) && dialogueText.text != "")
-                    {
-                        StartCoroutine(FermerEtLancerMessageAuto());
-                        if(indexListeDiag < listeDiag.Count) indexListeDiag++;
-                    }
+
+                    sourisPos = Input.mousePosition;
                 }
+
+            }
+
+            // 2. verif de l'accroupissement
+            else if (indexListeDiag == 1)
+            {
+                // On veut que le joueur s'accroupit et se redresse
+                if (Input.GetKeyDown(KeyCode.LeftControl))
+                {
+                    if (compteAccroupi < 2f) compteAccroupi++;
+                }
+
+                // Avec 2 clics, le joueur passe au suivant
+                if (compteAccroupi == 2f)
+                {
+                    StartCoroutine(FermerEtLancerMessageAuto());
+                    indexListeDiag++;
+                }
+            }
+
+            // 3. verif du saut
+            else if (indexListeDiag == 2)
+            {
+                // Le joueur doit appuyez ESPACE
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    StartCoroutine(FermerEtLancerMessageAuto());
+                    indexListeDiag++;
+                }
+            }
+
+            // 5. verif du sprint
+            else if (indexListeDiag == 4)
+            {
+                // Le joueur doit appuyez sur SHIFT
+                if (Input.GetKeyUp(KeyCode.LeftShift))
+                {
+                    StartCoroutine(FermerEtLancerMessageAuto());
+                    indexListeDiag++;
+                }
+            }
+
+            /* Dans tout autre cas, le clic est suffisant pour passer du texte de dialogue */
+            else
+            {
+                // Lorsque le le joueur clic apres que le dialogue n'est pas nul
+                if (Input.GetMouseButtonDown(0) && dialogueText.text != "")
+                {
+                    // Le message se ferme, et l'index augmente s'il y a un autre texte
+                    StartCoroutine(FermerEtLancerMessageAuto());
+                    if (indexListeDiag < listeDiag.Count) indexListeDiag++;
+                }
+            }
 
             }
     }
@@ -160,6 +239,7 @@ public class gestionPopups : MonoBehaviour
         }
     }
 
+    /* Fonction pour charger l'entierete d'un texte apres un clic */
     void ChargerTexteEntierEarly()
     {
         StopAllCoroutines();
