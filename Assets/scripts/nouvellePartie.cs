6dcf6fdbd31494937ad9f28713c7b42afa09367d
Tseng, Yun Yeh <e2483206@cmaisonneuve.qc.ca>
2026-05-25 21:42:14 -0400

    fias desbugs

--- Assets/scripts/nouvellePartie.cs
+++ Assets/scripts/nouvellePartie.cs
@@ -1,16 +1,59 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+using System.Collections;
 
 public class nouvellePartie : MonoBehaviour
 {
+    [Header("Fade")]
+    public GameObject fadeCanvas;
+    public Animator fadeAnimator;
+
+    [Header("Video")]
+    public GameObject videoObject;
+
+    public VideoPlayer videoPlayer;
     // Méthode appelée lors du clic sur le bouton "Nouvelle Partie"
     public void OnNouvellePartieClicked()
     {
         PlayerPrefs.DeleteKey("SauvegardeScene");
+
         // supprimer résultat fin
         PlayerPrefs.DeleteKey("BonFin");
-        CancelInvoke(("LoadSceneJeu"));
-        Invoke(("LoadSceneJeu"), 3f);
+
+        StartCoroutine(SequenceIntro());
+    }
+
+    IEnumerator SequenceIntro()
+    {
+        // activer fade
+        fadeCanvas.SetActive(true);
+
+        // écran devient noir
+        fadeAnimator.SetTrigger("FadeOut");
+
+        yield return new WaitForSeconds(3f);
+        // montrer vidéo
+        videoObject.SetActive(true);
+        // jouer vidéo
+        videoPlayer.Play();
+
+        // cacher image noire
+        fadeCanvas.SetActive(false);        
+
+        // attendre vidéo
+        yield return new WaitForSeconds(10f);
+
+        // remettre fade
+        fadeCanvas.SetActive(true);
+
+        // fade vers jeu
+        fadeAnimator.SetTrigger("FadeOut");
+
+        yield return new WaitForSeconds(1.5f);
+
+        // charger jeu
+        LoadSceneJeu();
     }
 
     private void LoadSceneJeu()
